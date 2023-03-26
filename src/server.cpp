abbb6a36906d5e0d5173eb8e2e4d78dd34b146a4
kooooootb <zavgmaxden@mail.ru>
2023-03-26 17:31:04 +0300

    pool size input and check

--- src/server.cpp
+++ src/server.cpp
@@ -3,7 +3,28 @@
 #include "Server.h"
 
 int main(){
-    Server server(32);
+    int poolSize;
+    const char *mes = "Enter pool size: ";
+    do {
+        std::cout << mes;
+        std::string input;
+        std::getline(std::cin, input);
+        try{
+            poolSize = std::stoi(input);
+        } catch(std::out_of_range &ex){
+            mes = "Too big number, try again: ";
+            poolSize = 0;
+            continue;
+        } catch(std::invalid_argument &ex){
+            mes = "Invalid data type, try again: ";
+            poolSize = 0;
+            continue;
+        }
+
+        mes = "Wrong pool size, try again: ";
+    }while(poolSize < 1);
+
+    Server server(poolSize);
 
     do{
         server.processRequest();
